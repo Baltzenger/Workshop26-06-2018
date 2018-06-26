03b8df510d6ee08438f3e12b119d74cfd409a985
aubry <aboucard@beable.fr>
2018-06-26 14:02:55 +0200

    master: add content in the home page

--- index.php
+++ index.php
@@ -14,8 +14,15 @@ echo '<!DOCTYPE html>
                 <img src="banner.jpg">
             </div>
             <div>
-                <h1>Notre plateforme, notre concepte</h1>
-                <p>ToutLeMondeC est une plateforme d\'enseignemment collaborative qui part du principe qu</p>
+                <h1>Notre plateforme, notre concept</h1>
+                <p>ToutLeMondeC est une plateforme d\'enseignemment collaborative qui part du principe que tout le
+                 monde à des connaissances et qu\'elles méritent d\'être partagées.<br/> C\'est donc ici que VOUS 
+                 intervennez !
+                </p>
+                <p>
+                    Afin que cette plateforme soit alimentée, nous avons besoin de votre participation. Vous aussi, 
+                    comme tout le monde, vous avez des connaissances qui peuvent être utiles à tout un cha
+                </p>
             </div>
         </div>
     </body>
