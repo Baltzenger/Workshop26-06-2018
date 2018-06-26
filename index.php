9c3d2fe42ecb8652979baa4f537fa13ae6bca82b
aubry <aboucard@beable.fr>
2018-06-26 11:51:15 +0200

    master: begin of mockup

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+echo '<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Acceuil</title>
+        <link type="text/css" href="style.css">
+    </head>
+    <body>
+        <div id="content">
+            '.require ('menu.php').'
+            <div id="banner">
+                <img src="banner.jpg">
+            </div>
+            <div>
+                <h1>Notre plateforme, notre concepte</h1>
+                <p>ToutLeMondeC est une plateforme d\'enseignemment collaborative qui part du principe qu</p>
+            </div>
+        </div>
+    </body>
+</html>';
