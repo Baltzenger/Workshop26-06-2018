d3845cb529f21841de7c69c62aae580c6c4d1caf
LuminasOne <boynick@hotmail.fr>
2018-06-27 15:25:06 +0200

    Nouveau header

---
+++ indexv2.php
@@ -0,0 +1,47 @@
+<?php
+
+echo '';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" type="text/css" href="public/style.css">
+</head>
+    <body>
+        <div id="content">
+            <?php require ('menu.php') ?>
+            <div id="banner">
+                <p class="slogan">  "Toute le monde a une connaissances, qui mérite d'être partager"</p>
+            </div>
+            
+
+                   
+
+                <h1>Notre plateforme, notre concept</h1>
+                <p>Learn2Share est une plateforme d'enseignemment collaborative qui part du principe que tout le
+                    monde à des connaissances et qu'elles méritent d'être partagées.<br/> C'est donc ici que VOUS
+                    intervennez !
+                </p>
+                <p>
+                    Afin que cette plateforme soit alimentée, nous avons besoin de votre participation. Vous aussi,
+                    comme tout le monde, vous avez des connaissances qui peuvent être utiles à tout un chacun 
+                </p>
+                <h1>Nos valeurs</h1>
+                <div id="knowledge">
+                    <span class="value"></span>
+                    <span>Connaissances</span>
+                </div>
+                <div id="knowledge">
+                    <span class="value"></span>
+                    <span>Partage</span>
+                </div>
+                <div id="knowledge">
+                    <span class="value"></span>
+                    <span>Coopération</span>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
