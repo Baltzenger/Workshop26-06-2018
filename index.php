6fe737282fa5704cae9bcbe868d0ae7d9f3ef18e
aubry <aboucard@beable.fr>
2018-06-27 16:02:49 +0200

    master: add DB connaction

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
-?>
-<!DOCTYPE html>
+echo '<!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
@@ -10,18 +9,18 @@
 </head>
     <body>
         <div id="content">
-            <?php require ('menu.php') ?>
+            <?php require (\'menu.php\') ?>
             <div id="banner">
             </div>
             <div>
                 <h1>Notre plateforme, notre concept</h1>
-                <p>Learn2Share est une plateforme d'enseignemment collaborative qui part du principe que tout le
-                    monde à des connaissances et qu'elles méritent d'être partagées.<br/> C'est donc ici que VOUS
+                <p>Learn2Share est une plateforme d\'enseignemment collaborative qui part du principe que tout le
+                    monde à des connaissances et qu\'elles méritent d\'être partagées.<br/> C\'est donc ici que VOUS
                     intervennez !
                 </p>
                 <p>
                     Afin que cette plateforme soit alimentée, nous avons besoin de votre participation. Vous aussi,
-                    comme tout le monde, vous avez des connaissances qui peuvent être utiles à tout un cha
+                    comme tout le monde, vous avez des connaissances qui peuvent être utiles à tout un chacun.
                 </p>
                 <h1>Nos valeurs</h1>
                 <div id="knowledge">
@@ -40,3 +39,4 @@
         </div>
     </body>
 </html>
+';
